13750ac0f99dad8976dcc2d49c43092b6b5dfedb
Hammarn <rickard.hammaren@scilifelab.se>
2017-10-13 11:26:36 +0200

    added sample name to md5 check

--- ngi_pipeline/engines/piper_ngi/launchers.py
+++ ngi_pipeline/engines/piper_ngi/launchers.py
@@ -393,10 +393,10 @@ def sbatch_piper_sample(command_line_list, workflow_name, project, sample,
     sbatch_text_list.append("\nPIPER_RETURN_CODE=$?")
 
     #Precalcuate md5sums
-    sbatch_text_list.append('MD5FILES="$SNIC_TMP/ANALYSIS/{}/piper_ngi/05_processed_alignments/*.bam'.format(project.project_id))
+    sbatch_text_list.append('MD5FILES="$SNIC_TMP/ANALYSIS/{}/piper_ngi/05_processed_alignments/*{}*.bam'.format(project.project_id,sample.name))
     sbatch_text_list.append('$SNIC_TMP/ANALYSIS/{}/piper_ngi/05_processed_alignments/*.table'.format(project.project_id))
-    sbatch_text_list.append('$SNIC_TMP/ANALYSIS/{}/piper_ngi/07_variant_calls/*.genomic.vcf.gz'.format(project.project_id))
-    sbatch_text_list.append('$SNIC_TMP/ANALYSIS/{}/piper_ngi/07_variant_calls/*.annotated.vcf.gz"'.format(project.project_id))
+    sbatch_text_list.append('$SNIC_TMP/ANALYSIS/{}/piper_ngi/07_variant_calls/*{}*.genomic.vcf.gz'.format(project.project_id,sample.name))
+    sbatch_text_list.append('$SNIC_TMP/ANALYSIS/{}/piper_ngi/07_variant_calls/*{}*.annotated.vcf.gz"'.format(project.project_id,sample.name))
     sbatch_text_list.append('for f in $MD5FILES')
     sbatch_text_list.append('do')
     sbatch_text_list.append("    md5sum $f | awk '{printf $1}' > $f.md5 &")
