ff2013a69d18dfec8f7a4b4b940ab7ee97de5ff8
b97pla <pontus.larsson@medsci.uu.se>
2020-04-24 17:02:26 +0200

    docker and singularity image scripts

---
+++ docker/build_docker_and_singularity.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -l
+
+## Script for building a docker image and subsequently a singularity image
+## you'll probably need Singularity 3+ in order to use docker-daemon
+
+# remove any pre-existing singularity images
+rm -f ngi_pipeline.sif
+
+# build docker image
+docker build -t ngi_pipeline:latest .
+
+# build singularity image from docker imnage
+singularity build ngi_pipeline.sif docker-daemon:ngi_pipeline:latest
---
+++ docker/singularity_ngi_pipeline.sh
@@ -0,0 +1,23 @@
+#! /bin/bash -l
+
+## example script for interacting with the singularity image and setting the
+## appropriate environment variables and bind points
+##
+## an example config is present under /conf
+##
+## note that for e.g. development, you can bind the ngi_pipeline source code
+## under /ngi_pipeline and it will run off that
+##
+## example usage: singularity_ngi_pipeline.sh ngi_pipeline_start.py -h
+##
+
+NGI_CONFIG=/conf/ngi_config.yaml
+CHARON_BASE_URL=this-is-charon-url
+CHARON_API_TOKEN=this-is-charon-token
+
+singularity exec --bind \
+#/path/to/dev/src/code:/ngi_pipeline,\
+/lupus/ngi/staging/latest/sw:/sw,\
+/lupus/ngi/staging/latest/resources:/resources,\
+/lupus/ngi/staging/wildwest/ngi2016001:/data/ngi2016000 \
+ngi_pipeline.sif $@
